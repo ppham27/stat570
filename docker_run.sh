12677f6d6f72b1a59c63b6bc328004ba65830b0b
Philip Pham <philip.pham2@gmail.com>
2018-11-06 17:59:16 -0800

    made progress on hw6 problem 2

--- docker_run.sh
+++ docker_run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-docker run -p 8888:8888 -p 6006:6006 -it --rm \
+docker run -p 8888:8888 -p 6006:6006 -p 6007:6007 \
+       -it --rm \
        -v "$(pwd):/local" \
        -w "/local" \
        stat570:latest \
